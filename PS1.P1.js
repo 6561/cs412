cc041bda93b172440890e70dfa3df1b4a2ad6374
Bart Considine <bartjc@bu.edu>
2021-09-27 17:17:00 -0400

    Edited revAlph

--- PS1.P1.js
+++ PS1.P1.js
@@ -1,18 +1,21 @@
-function revAlph(str) {
-    if (str.length<2){
+console.log("say cheese!")
+console.log(revAlph("exioi"))
+console.log("More cheese?")
+function revAlph(st) {
+    console.log("In the function")
+    if (st.length<2){
         return str
     }
-    
     ret = ""
-    for (let i=0;i <str.length;i++) {
-        current=str.charAt(i)
-        for (let j=0;j<str.length;j++) {
-            
+    for (let i=0;i <st.length;i++) {
+        current=st.charAt(i)
+        for (let j=0;j<st.length;j++) {
             if (j==ret.length) {
                 ret+=current
                 break
-            } else if (j<ret.length) {
-                if (ret.charAt(j))
+            } else if (j<ret.length && ret.charAt(j)>=current) {
+                ret=ret.substring(0,j)+current+ret.substring(j)
+                break
             }
         }
     }
