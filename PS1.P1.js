65729d02b51b4fd399beb12f201cce75138b6742
Bart Considine <bartjc@bu.edu>
2021-09-27 17:33:36 -0400

    Got revAlph to function as intended

--- PS1.P1.js
+++ PS1.P1.js
@@ -1,19 +1,20 @@
-console.log("say cheese!")
-console.log(revAlph("exioi"))
-console.log("More cheese?")
+const res = st1 => revAlph(st1)
+console.log(`exioi in reverse alphabetical order is ${res("exioi")}`)
+console.log(`supercalifragilisticexpialidocious in reverse alphabetical order is ${res("supercalifragilisticexpialidocious")}`)
+
 function revAlph(st) {
-    console.log("In the function")
-    if (st.length<2){
+    len = st.length
+    if (len<2){
         return str
     }
     ret = ""
-    for (let i=0;i <st.length;i++) {
+    for (let i=0;i <len;i++) {
         current=st.charAt(i)
-        for (let j=0;j<st.length;j++) {
+        for (let j=0;j<len;j++) {
             if (j==ret.length) {
                 ret+=current
                 break
-            } else if (j<ret.length && ret.charAt(j)>=current) {
+            } else if (j<ret.length && ret.charAt(j)<=current) {
                 ret=ret.substring(0,j)+current+ret.substring(j)
                 break
             }
