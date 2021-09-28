0ad6c047fbdf985a6030c7d0b9a7370fb896eea0
Bart Considine <bartjc@bu.edu>
2021-09-27 22:06:06 -0400

    Fixed remaining issues with PS1.P3.js

--- PS1.P3.js
+++ PS1.P3.js
@@ -4,6 +4,8 @@ function doIt(str, dec) {
 
 e1 = doIt("supercalifragilisticexpialidocious", sj => cArray(sj))
 console.log(e1)
+e2 = doIt("supercalifragilisticexpialidocious", sj => dArray(sj))
+console.table(e2)
 
 function cArray(sc) {
     li =[]
@@ -12,7 +14,6 @@ function cArray(sc) {
     }
     current=""
     for (let i=0;i<sc.length;i++) {
-
         if (sc[i]=="c" || sc[i]=='c') {
             li.push(current)
             current="c"
@@ -22,4 +23,23 @@ function cArray(sc) {
     }
     li.push(current)
     return li
+}
+
+function dArray(sd) {
+    current2=""
+    count=0
+    for (let j=0;j<sd.length;j++) {
+        if (sd[j]=="a" || sd[j]=='a') {
+            current2+="A"
+        } else {
+            current2=current2+sd[j]
+            count++
+        }
+    }
+    return {
+        originalString: sd,
+        modifiedString: current2,
+        numberReplaced: count,
+        length: current2.length,
+    }
 }
